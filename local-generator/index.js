4d2e108de4a70d3744686101de96f7f19ce6de37
Jeevan James <jeevanjj@live.com>
2017-01-15 15:47:16 -0500

    Updated to use the new swagen-angular generator. Added some more comments to the local generator.

--- local-generator/index.js
+++ local-generator/index.js
@@ -1,4 +1,19 @@
 /**
+ * This is a sample local Swagen generator. A local generator is not installed as a NPM package, but
+ * can be part of a Node project. This allows your project to have a custom generator without needing
+ * to publish a separate generator to the NPM repository.
+ *
+ * A local generator is structurally similar to a packaged generator. It exports the same structure.
+ * The difference is in how the generator is specified in the Swagen configuration file.
+ * Similar to the require keyword, if you use a relative path for the generator name, Swagen will
+ * assume it is a local generator and handle it accordingly.
+ *
+ * See the swagen.config.json file (petstore-local-generator entry) for an example.
+ *
+ * This sample generator generates a Markdown file (.md) describing the web service.
+ */
+
+/**
  * Loading the swagen-core package to use the CodeBuilder class.
  * This step is optional, as the output can be manually generated, but the CodeBuilder class makes it easy.
  */
@@ -11,7 +26,9 @@ const swagenCore = require('swagen-core');
 const _ = require('lodash');
 
 /**
- * Modes supported by this generator. Local generators can support multiple modes, just like packaged generators.
+ * Modes supported by this generator. Local generators can support multiple modes, just like packaged
+ * generators. However, this has no practical value and it is easier to simply have a single mode per
+ * local generator.
  * For simplicity, this sample only uses a single mode.
  */
 const modes = [
@@ -50,6 +67,11 @@ function generate(definition, profile) {
     return cb.toCode();
 }
 
+/**
+ * Validates the generation profile specified in the configuration.
+ * In this sample, there is nothing to validate.
+ * @param {Profile} profile - Generation profile
+ */
 function validateProfile(profile) {
     return true;
 }
